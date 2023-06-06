19ab68a079479e2c605655d4b86be66a947e1836
simja <simja@example.com>
2023-06-05 18:43:27 -0600

    sistema de Login diseño y funcionalidad

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +8,56 @@
     <title>Home</title>
 
     <?php include_once './components/styles.php' ?>
+    <link rel="stylesheet" href="./css/index.css">
 </head>
+
 <body>
-    <h1>Hello world</h1>
+    <div class="col-md-6 form-background">
+        <div class="col-md-4 mx-auto">
+            <div class="">
+                <div class="card-header text-center text-light">
+                    <i class="fa-solid fa-user logo-user mt-4"></i>
+                    <h2>Iniciar sesion</h2>
+                </div>
+                <div id="alert">
+                    
+                </div>
+                <form class="card-body" id="form">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            <i class="fa-solid fa-user"></i>
+                        </span>
+                        <input type="text" class="form-control bg-blur" placeholder="RFC" id="rfc" required>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            <i class="fa-solid fa-key"></i>
+                        </span>
+                        <input type="password" class="form-control bg-blur" placeholder="Password" id="password" required>
+                    </div>
+
+                    <div class="d-grid mx-auto">
+                        <div class="mx-auto mt-4 mb-4 loader visually-hidden" id="loader">
+                            <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+                            </div>
+                            <span>Cargando ...</span>
+                        </div>
+                        <button type="submit" id="btn-login"
+                            class="btn btn-primary d-flex justify-content-between align-items-center ">
+                            Iniciar sesion
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </button>
+                    </div>
+                </form>
+                <div class="card-footer mt-4">
+                    <p class="text-light">Olvidaste tu contraseña, <a href="/">Recuperala aqui.</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./js/index.js"></script>
 </body>
+
 </html>
---
+++ js/index.js
@@ -0,0 +1,67 @@
+const form = document.getElementById("form");
+const loader = document.getElementById("loader");
+const btnLogin = document.getElementById("btn-login");
+const rfc = document.getElementById("rfc");
+const password = document.getElementById("password");
+const alertCSS = document.getElementById("alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (rfc.value === "" || password.value === "") {
+    alertCSS.innerHTML = `<div class="alert alert-danger " role="alert">
+                            Los campos no pueden estar vacios
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+    return;
+  }
+
+  if (rfc.value.length > 13) {
+    alertCSS.innerHTML = `<div class="alert alert-danger d-flex" role="alert">
+                            El RFC no puede ser mayor a 13 caracteres
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+    return;
+  }
+
+  console.log("enviando formulario...");
+  loader.classList.remove("visually-hidden");
+  btnLogin.classList.add("visually-hidden");
+
+  iniciarSesion();
+});
+
+const iniciarSesion = () => {
+  const dataUser = {
+    rfc: rfc.value,
+    password: password.value,
+  };
+
+  postData("http://localhost:3000/api/login", dataUser)
+    .then((data) => {
+      console.log(data);
+      data.status === "accepted"
+        ? (location.href = "./products.php")
+        : (alertCSS.innerHTML = `<div class="alert alert-danger d-flex justify-content-between" role="alert">
+                                ${data.message}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>`);
+    })
+    .finally(() => {
+      loader.classList.add("visually-hidden");
+      btnLogin.classList.remove("visually-hidden");
+    })
+    .catch((err) => console.log("Solicitud fallida", err));
+};
+
+async function postData(url = "", data = {}) {
+  // Opciones por defecto estan marcadas con un *
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors", 
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
