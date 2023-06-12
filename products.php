165865024d8c3b41bb58cfe2bc01ab3682268fd3
simja <simja@example.com>
2023-06-11 19:05:52 -0600

    Tabla productos

--- products.php
+++ products.php
@@ -24,6 +24,42 @@
                 <?php include_once './components/header.php'  ?>
                 <div class="container mt-2">
                     <h1>dashboard products</h1>
+                    <div class="d-flex justify-content-end my-2">
+                        <button type="button" class="btn btn-primary btn-lg d-grid btn-add">
+                            <p>Nuevo Producto</p>
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+                    </div>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </section>
         </container>
