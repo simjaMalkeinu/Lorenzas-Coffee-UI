f5bbbb0af679efd0471baeb83323b0d5a355acee
simja <simja@example.com>
2023-06-13 17:08:26 -0600

    Navegacion correcion

--- components/navigation.php
+++ components/navigation.php
@@ -7,9 +7,33 @@
                     <i class="fa-solid fa-shop"></i>
                     <p>Ventas</p>
                 </li>
-                <li>
+                <li class="accordion-flush">
                     <i class="fa-solid fa-warehouse"></i>
-                    <p>Inventario</p>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                aria-controls="flush-collapseOne">
+                                <p>Inventarios</p>
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse"
+                            data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <ul>
+                                    <li class="inventario">
+                                        <i class="fa-brands fa-product-hunt"></i>
+                                        <p>Productos</p>
+                                    </li>
+                                    <li class="inventario">
+                                        <i class="fa-solid fa-hippo"></i>
+                                        <p>Insumos</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </li>
                 <li>
                     <i class="fa-solid fa-bars-progress"></i>
@@ -21,9 +45,7 @@
                             <i class="fa-solid fa-users"></i>
                             <p>Users</p>
                         </li>';
-                }
-                
-                ?>
+                }?>
 
                 <li>
                     <i class="fa-solid fa-folder-open"></i>
@@ -33,6 +55,7 @@
                     <i class="fa-solid fa-user"></i>
                     <p>Cuenta</p>
                 </li>
+
             </div>
             <a href="/logout" class="d-grid">
                 <button class="btn bg-primary text-light d-grid logout">
